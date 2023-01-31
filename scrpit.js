abc91ae4e2c3e2a9ac866ce26cbcc4e3be006a6e
elcougario <b.godolphin@mail.utoronto.ca>
2023-01-31 15:47:33 -0500

    Website Lab 1
    hello world!

---
+++ scrpit.js
@@ -0,0 +1,8 @@
+function clicked() {
+    console.log("You clicked me")
+   }
+   //Event listener function
+var button = document.getElementById("my-btn");
+button.addEventListener("click", function(){
+ alert("You clicked me!");
+});
